73686d4bd25ba108647894c8b8a46e6bb802f64d
Gasfar Mukhametdinov <Gasfar.Mukhametdinov.GDC@ts.fujitsu.com>
2017-06-14 20:04:04 +0300

    Connector and simple console chat added

---
+++ ApiAiTestLab/ApiAiTestLab/ApiAiBot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+using System.Configuration;
+using ApiAiSDK;
+using ApiAiSDK.Model;
+
+namespace ApiAiTestLab
+{
+    public class ApiAiBot
+    {
+        readonly NameValueCollection appSettings;
+        readonly AIConfiguration apiAiConfiguration;
+        readonly ApiAi apiAi;
+        public ApiAiBot()
+        {
+            appSettings = ConfigurationManager.AppSettings;
+            apiAiConfiguration = new AIConfiguration(appSettings["ClientToken"], SupportedLanguage.English);
+            apiAi = new ApiAi(apiAiConfiguration);
+        }
+        public string GetResponse(string userPhrase)
+        {
+            var response = apiAi.TextRequest(userPhrase);
+            return response.Result.Fulfillment.Speech;
+        }
+    }
+}
---
+++ ApiAiTestLab/ApiAiTestLab/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using ApiAiSDK;
+using ApiAiSDK.Model;
+
+namespace ApiAiTestLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testBot = new ApiAiBot();
+            var continueTalking = true;
+            while (continueTalking)
+            {
+                Console.Write("You: ");
+                var userRequest = Console.ReadLine();
+                if (userRequest == "exit")
+                {
+                    continueTalking = false;
+                    break;
+                }
+                var botResponse = testBot.GetResponse(userRequest);
+                Console.WriteLine("Bot: {0}", botResponse);
+            }
+        }
+    }
+}
